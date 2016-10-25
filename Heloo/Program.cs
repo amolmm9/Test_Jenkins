f80bf0d81a9c92f2a13fab63fb0761d15e68d155
amolmm9 <amolmm9@gmail.com>
2016-10-25 15:09:17 +0530

    aa

--- Heloo/Program.cs
+++ Heloo/Program.cs
@@ -11,7 +11,7 @@ namespace Heloo
         static void Main(string[] args)
         {
             Console.WriteLine("Aaa");
-            Console.WriteLine("Aaa");
+            Console.WriteLine("Yes");
             Console.Read();
         }
     }
